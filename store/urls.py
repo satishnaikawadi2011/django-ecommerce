b1e991412d1e8cf80fabbdf9d808c784686df1f5
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-10-20 11:44:40 +0530

    added middleware in urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from  .views import home,register,login,cart,checkout,order
+from .middlewares.auth import auth_middleware
 
 urlpatterns = [
     path('',home.index,name='homePage'),
@@ -8,7 +9,7 @@ urlpatterns = [
     path('logout',login.logout,name='logout'),
     path('cart',cart.Cart.as_view(),name='cart'),
     path('checkout',checkout.Checkout.as_view(),name='checkout'),
-    path('orders',order.OrderView.as_view(),name='orders'),
+    path('orders',auth_middleware(order.OrderView.as_view()),name='orders'),
     path('add_to_cart',home.add_to_cart,name="add_to_cart"),
     path('increament_cart_quantity',home.increament_cart_quantity,name="increament_cart_quantity"),
     path('decreament_cart_quantity',home.decreament_cart_quantity,name='decreament_cart_quantity')
