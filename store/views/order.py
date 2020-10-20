8e7a0cb8accaba6665807e4b8d860e91caea92de
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-10-20 11:36:06 +0530

    added auth middleware

--- store/views/order.py
+++ store/views/order.py
@@ -3,10 +3,12 @@ from ..models.product import Product
 from django.views import View
 from ..models.order import Order 
 from ..models.customer import Customer
-
+from ..middlewares.auth import auth_middleware
+from  django.utils.decorators import method_decorator
 
 
 class OrderView(View):
+    @method_decorator(auth_middleware)
     def get(self,request):
         customer = request.session.get('customer_id')
         orders = Order.get_orders_by_customer(customer)
